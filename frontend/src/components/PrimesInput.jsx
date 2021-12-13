f81038d3cdeefdbbf75b24789c8faa9135c381eb
kisa7081 <41703406+kisa7081@users.noreply.github.com>
2021-12-12 20:36:03 -0600

    Changes to the input tag.

--- frontend/src/components/PrimesInput.jsx
+++ frontend/src/components/PrimesInput.jsx
@@ -44,7 +44,7 @@ class PrimesInput extends Component {
 	         	 Enter a number to find all prime numbers less than that number (maximum 1000): 
 	         	 <input type="text" name='primeInput' value={this.state.value} onChange={this.handleChange} onKeyDown={this.handleKeyDown}/>
 	        	</label>
-	        	<input value="Submit"  />
+	        	<input type="submit" defaultValue="Submit"  />
 	      	</form>
 	      	<PrimesDisplay primes={this.state}/>
 	     </div>
